2f5cf7982375bd524ac84c1cc0e11506c6e4fd5c
root <trj08012002@gmail.com>
2025-05-16 13:40:48 +0530

    handling error

--- Task 2/app.py
+++ Task 2/app.py
@@ -6,20 +6,23 @@ app = Flask(__name__)
 def home():
     return "<h1>Welcome</h1>"
 
-@app.route('/<int:num1>/<int:num2>', methods=['GET', 'POST'])
+@app.route('/calculate/<num1>/<num2>', methods=['GET', 'POST'])
 def calculate(num1, num2):
-    # Avoid division by zero
-    if num2 == 0:
-        division_result = "Undefined (division by zero)"
+    if num1.isnumeric() and num2.isnumeric():
+        num1,num2=int(num1),int(num2)
+        if num2 == 0:
+            division_result = "Undefined (division by zero)"
+        else:
+            division_result = num1 / num2
+        return f"""
+            Addition: {num1 + num2}<br>
+            Subtraction: {num1 - num2}<br>
+            Multiplication: {num1 * num2}<br>
+            Division: {division_result}
+            """
     else:
-        division_result = num1 / num2
+        return "Invalid data"
 
-    return f"""
-    Addition: {num1 + num2}<br>
-    Subtraction: {num1 - num2}<br>
-    Multiplication: {num1 * num2}<br>
-    Division: {division_result}
-    """
 
 if __name__ == "__main__":
     app.run(debug=True, port=3010, host="0.0.0.0")
